b82fa66a5d27730219b55795e738661e6b599b53
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-04 12:39:58 +0100

    Removed unused variable

--- src/main/kotlin/no/nav/helse/sykepenger/Beregningsgrunnlag.kt
+++ src/main/kotlin/no/nav/helse/sykepenger/Beregningsgrunnlag.kt
@@ -2,4 +2,4 @@ package no.nav.helse.sykepenger
 
 import java.time.LocalDate
 
-data class Beregningsgrunnlag(val søknad: Søknad, val grunnbeløp: Long, val sykmeldingsgrad: Int, val sykepengegrunnlag: Sykepengegrunnlag, val sisteUtbetalingsdato: LocalDate)
+data class Beregningsgrunnlag(val søknad: Søknad, val sykmeldingsgrad: Int, val sykepengegrunnlag: Sykepengegrunnlag, val sisteUtbetalingsdato: LocalDate)
